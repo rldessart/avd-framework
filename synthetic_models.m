f70a7a6b86d1534947eab027da2af969537e244b
rldessart <rldessart@gmail.com>
2026-04-22 10:03:39 -0300

    synthetic_models.m

--- synthetic_models.m
+++ synthetic_models.m
@@ -1,49 +1,34 @@
 function [T, dx] = synthetic_models(model_type)
 
-  % ============================================
-  % GRID
-  % ============================================
   nx = 100;
   nz = 100;
   dx = 1;
 
   [X,Z] = meshgrid(1:nx,1:nz);
 
-  % ============================================
-  % INITIALIZE MODEL
-  % ============================================
   T = zeros(nz,nx);
 
-  % ============================================
-  % SELECT MODEL
-  % ============================================
   switch model_type
 
-    case 1  % Buried circular body
+    case 1
       T((X-50).^2 + (Z-50).^2 <= 15^2) = 1;
 
-    case 2  % Vertical dike
-      T(:,45:55) = 1;
+    case 2
+      T(:,40:60) = 10;
 
-    case 3  % Vertical contact
-      T(:,50:end) = 1;
+    case 3
+      T(:,50:end) = 10;
 
     otherwise
       error('Invalid model type')
   end
 
-  % ============================================
-  % SIMULATE MAGNETIC RESPONSE (SMOOTHING)
-  % ============================================
-  G = gaussian_kernel(9, 2);
+  G = gaussian_kernel(5, 1);
   T = conv2(T, G, 'same');
 
 end
 
 
-% ============================================
-% GAUSSIAN KERNEL FUNCTION
-% ============================================
 function G = gaussian_kernel(sz, sigma)
   [x,y] = meshgrid(-floor(sz/2):floor(sz/2));
   G = exp(-(x.^2 + y.^2)/(2*sigma^2));
